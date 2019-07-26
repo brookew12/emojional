ee89a78d1948a9a13957f9182b4a4a2f20773a0e
Apple <apple@Apples-MacBook-Pro-35.local>
2019-07-26 09:58:36 -0400

    Complete Iteration 0

---
+++ emojional/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  emojional
+//
+//  Created by Apple on 7/25/19.
+//  Copyright © 2019 KWK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+//    @IBAction func book(_ sender: UIButton) {
+//        print("keep working!")
+//    }
+//    
+//    
+//    @IBAction func massage(_ sender: UIButton) {
+//    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
