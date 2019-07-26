9eda372122fb1e28bdfa526d993ac7c1a3d61d81
Apple <apple@Apples-MacBook-Pro-35.local>
2019-07-26 11:44:11 -0400

    Complete Iteration 11:44AM

--- emojional/ViewController.swift
+++ emojional/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 
   let emojis = ["📖" : "book", "💆🏾‍♀️" : "massage"]
-let customMessages = ["book" : ["keep working!", "work harder!", "don't give up!"], "massage" : ["take a break!", "go watch something", "eat food!!!"]]
+let customMessages = ["book" : ["keep working!", "work harder!", "don't give up!", "don't you want to go to college", "just keep reading, just keep reading reading reading"], "massage" : ["take a break!", "go watch something", "eat food!!!", "you deserve to chill", "pat yourself on the back"]]
 
 class ViewController: UIViewController {
 
@@ -21,7 +21,7 @@ class ViewController: UIViewController {
         
           let selectedEmotion = sender.titleLabel?.text
       
-        let emojiMessage = customMessages[emojis[selectedEmotion!]!]?[Int.random(in: 0...2)]
+        let emojiMessage = customMessages[emojis[selectedEmotion!]!]?[Int.random(in: 0...4)]
         
         let alertController = UIAlertController(title: "Let Emojional give you advice!", message: emojiMessage, preferredStyle: UIAlertController.Style.alert)
         alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
